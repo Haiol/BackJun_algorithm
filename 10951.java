dc9b7c829946ac582d93bc6d92fa078fb2be8871
Haiol <41848169+Haiol@users.noreply.github.com>
2021-06-09 18:48:14 +0900

    Create 10951.java

---
+++ 10951.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class 10951 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            while (true) {
+                int result=0;
+                String s = br.readLine();
+                String[] arr = s.split(" ");
+                for(int i=0;i< arr.length;i++)
+                    result+=Integer.valueOf(arr[i]);
+                bw.write(result+ "\n");
+                bw.flush();
+            }
+        }catch (Exception e){
+        }
+    }
+}
