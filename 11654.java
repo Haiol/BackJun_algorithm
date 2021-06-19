7ba94e690328dda67722d6b25d996c50550ab5c4
Haiol <41848169+Haiol@users.noreply.github.com>
2021-06-19 21:36:32 +0900

    Create 11654.java

---
+++ 11654.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class 11654 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String tmp = br.readLine();
+
+            bw.write((int)tmp.charAt(0)+"\n");
+            bw.flush();
+
+        } catch (IOException e) {
+        }
+    }
+
+}
