32f17990caf95fa156978260c11615310dcf6f64
Haiol <41848169+Haiol@users.noreply.github.com>
2021-06-26 16:08:27 +0900

    Create 1193.java

---
+++ 1193.java
@@ -0,0 +1,7 @@
+public class 1193{
+    public static void main(String[] args) {
+        int s = new java.util.Scanner(System.in).nextInt();
+        for(int i=0,c=0;c<s;i++) for(int j=i-1,q=1;q<i;j--,q++,c++) if(c==s-1)
+                    System.out.println(i%2==0?j+"/"+q:q+"/"+j);
+    }
+}
