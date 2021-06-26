e182791c09a1b8f8b2dac68d7da0f99d2aaef0c2
Haiol <41848169+Haiol@users.noreply.github.com>
2021-06-26 19:58:47 +0900

    Create 2869.java

---
+++ 2869.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class 2869{
+    public static void main(String[] args) throws IOException {
+        String arr[] = new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ");
+        int a= Integer.valueOf(arr[0]),b=Integer.valueOf(arr[1]),v=Integer.valueOf(arr[2]),r=(v-a)/(a-b);
+        System.out.println((a-b)*r+a>=v?r+1:r+2);
+    }
+}
