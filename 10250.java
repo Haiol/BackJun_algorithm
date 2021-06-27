a225288b005a7e22316c86fbac611b9f7b440e52
Haiol <41848169+Haiol@users.noreply.github.com>
2021-06-27 11:04:33 +0900

    Create 10250.java

---
+++ 10250.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class 10250{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+       while (0<s--){
+            int h = sc.nextInt(),w = sc.nextInt(),n = sc.nextInt();
+            System.out.println(n%h==0?h*100+n/h:n%h*100+n/h+1);
+        }
+    }
+}
