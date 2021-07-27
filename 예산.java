ee39a6685c4c907403dadd49899bc54736d3593c
Haiol <41848169+Haiol@users.noreply.github.com>
2021-07-27 22:27:19 +0900

    Create 예산.java

---
+++ 예산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0,sum=0;
+        Arrays.sort(d);
+        for(int i : d){
+            if(sum+i> budget)break;
+            sum+=i;
+            answer++;
+        }
+        return answer;
+    }
+}
