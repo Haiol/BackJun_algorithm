5d3ab13f80754e8aa8265f017269f5f47919183c
Haiol <41848169+Haiol@users.noreply.github.com>
2021-07-27 22:57:11 +0900

    Create 약수의갯수와덧셈.java

---
+++ 약수의갯수와덧셈.java
@@ -0,0 +1,14 @@
+class Solution {
+   public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <=right;i++)
+            if(medicineNumber(i)%2==0)answer+=i;
+            else answer-=i;
+        return answer;  
+    }
+    public int medicineNumber(int n){
+        int cnt=0;
+        for(int i=1;i<=n;i++)if(n%i==0)cnt++;
+        return cnt;
+    }
+}
