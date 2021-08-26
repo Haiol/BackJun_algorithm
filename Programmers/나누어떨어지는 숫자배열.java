dea902f9b3fb6cddc44ddf29a8d919a7ce41f5f4
Haiol <41848169+Haiol@users.noreply.github.com>
2021-08-26 21:49:54 +0900

    Create 나누어떨어지는 숫자배열.java

---
+++ Programmers/나누어떨어지는 숫자배열.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int answer[] = Arrays.stream(arr).filter(value -> value%divisor==0).sorted().toArray();
+
+        return  answer.length==0?new int[]{-1}:answer;
+    }
+}
