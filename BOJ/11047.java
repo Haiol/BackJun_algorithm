4d143e842fbeb11b1cb8cb7ead5be23018de5588
Haiol <41848169+Haiol@users.noreply.github.com>
2021-09-22 20:19:08 +0900

    동전0

---
+++ BOJ/11047.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main{
+    public void solution(){
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt(),money[] = new int[s],coin=0;
+        int total = sc.nextInt();
+        for(int i=0;i<s;i++)
+            money[i] = sc.nextInt();
+        for(int i = s-1;i>=0;i--)
+            if(money[i]<=total){
+                coin += total/money[i];
+                total %= money[i];
+            }
+        System.out.println(coin);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solution();
+    }
+
+}
